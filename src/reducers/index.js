1b9e9dc1a3c61bec3113fb65d5ad681d0bcb9f31
Pradeep Kumar <pkmaurya0801@gmail.com>
2021-07-28 00:00:28 +0530

    my redux app

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import changeTheNumber from "./upDown";
+
+import { combineReducers } from "redux";
+
+const rootReducers = combineReducers({
+    changeTheNumber
+});
+
+export default rootReducers;
